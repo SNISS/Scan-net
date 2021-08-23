a25eb55c544658b4c56ebed4201108bf12a4320e
SNISS <82043842+SNISS@users.noreply.github.com>
2021-08-23 16:12:55 -0300

    Add files via upload

---
+++ ping-sweep.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if ["$1" == ""] 
+
+then
+    echo "Modo de uso: ./ping-sweep.sh 192.168.0"
+
+else
+
+   for host in {1..254};
+
+do
+   ping -c1 $1.$host | grep "64 bytes"| cut -d ":" -f 1 | cut -d " " -f 4;done
+
+
+fi
